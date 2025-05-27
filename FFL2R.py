e9e7706f71889d218e754f2da14aa0ffde4c71db
Taeveril <39547209+Taeveril@users.noreply.github.com>
2025-05-27 18:43:46 -0400

    Shop rando in

--- FFL2R.py
+++ FFL2R.py
@@ -1,5 +1,6 @@
 import binascii
 import random
+from re import M
 import FFL2R_data
 import argparse
 from tkinter import Tk
@@ -38,80 +39,183 @@ def main(rom_path:str|None, seed:int|None):
             # this can probably be generalized if there's more bit overflow with additional scripts
             # but right now it only happens once so it's done by hardcoding
             if hexData[i*2:i*2+2].upper() == b"FF":
-                romData.update({ ("{:08X}".format(i)) : b"03" })
+                romData.update({ hex(i) : b"03" })
                 i+=1
-                romData.update({ ("{:08X}".format(i)) : b"53" })
+                romData.update({ hex(i) : b"53" })
             else:
-                romData.update({ ("{:08X}".format(i)) : bytes(hex(int((hexData[i*2:i*2+2]),16)+4)[2:].zfill(2).upper(), encoding='utf-8') })
+                romData.update({ hex(i) : bytes(hex(int((hexData[i*2:i*2+2]),16)+4)[2:].zfill(2).upper(), encoding='utf-8') })
         # insertion of new scripts
         # Ashura, Venus, and Odin all use the same drop script (one of each MAGI), with Venus + Odin dropping Aegis / Masmune in the post-battle script.
         # To increase randomization possibilities, new scripts for Ashura and Venus have been added.
         # Odin keeps the original script. All non-TrueEye MAGI can be randomized.
         elif i >= 180607 and i <= 190948:
             if i == 180607 or i == 180609:
-                romData.update({ ("{:08X}".format(i)) : b"69" })
+                romData.update({ hex(i) : b"69" })
             elif i == 180608:
-                romData.update({ ("{:08X}".format(i)) : b"E5" })
+                romData.update({ hex(i) : b"E5" })
             elif i == 180610:
-                romData.update({ ("{:08X}".format(i)) : b"FB" })
+                romData.update({ hex(i) : b"FB" })
             else:
-                romData.update({ ("{:08X}".format(i)) : hexData[(i-4)*2:(i-4)*2+2].upper() })
+                romData.update({ hex(i) : hexData[(i-4)*2:(i-4)*2+2].upper() })
         # Ashura's new custom MAGI drop script
         elif i >= 190949 and i<=190969:
             match i % 3:
                 case 0:
-                    romData.update({ ("{:08X}".format(i)) : b"0A" })
+                    romData.update({ hex(i) : b"0A" })
                 case 1:
-                    romData.update({ ("{:08X}".format(i)) : b"09" })
+                    romData.update({ hex(i) : b"09" })
                 case 2:
-                    romData.update({ ("{:08X}".format(i)) : b"19" })
+                    romData.update({ hex(i) : b"19" })
         elif i == 190970:
-            romData.update({ ("{:08X}".format(i)) : b"00" })
-            romData.update({ "00028EDB" : b"C0"})
+            romData.update({ hex(i) : b"00" })
+            romData.update({ hex(167643) : b"C0"}) #0x28edb
         # Venus' new custom MAGI drop script
         elif i >= 190971 and i<=190991:
             match i % 3:
                 case 0:
-                    romData.update({ ("{:08X}".format(i)) : b"19" })
+                    romData.update({ hex(i) : b"19" })
                 case 1:
-                    romData.update({ ("{:08X}".format(i)) : b"0A" })
+                    romData.update({ hex(i) : b"0A" })
                 case 2:
-                    romData.update({ ("{:08X}".format(i)) : b"09" })   
+                    romData.update({ hex(i) : b"09" })   
         elif i == 190992:
-            romData.update({ ("{:08X}".format(i)) : b"00" })
-            romData.update({ "0002CC71" : b"C1"}) #addr +4 offset
+            romData.update({ hex(i) : b"00" })
+            romData.update({ hex(183409) : b"C1"}) #addr +4 offset #0x2cc71
         else:
-            romData.update({ ("{:08X}".format(i)) : hexData[i*2:i*2+2].upper() })
+            romData.update({ hex(i) : hexData[i*2:i*2+2].upper() })
         i+=1
+     
 
     game = FFL2R_data.GameData
-    
+
     random.shuffle(game.treasures)
-
-
     j=0
     for address in game.treasureAddresses:
-        romData.update({ address : game.treasures[j] })
+        romData.update({ hex(address) : game.treasures[j] })
         if game.treasures[j] == b"FF":
-            k = "{:08X}".format(int("0x" + address,16)-2)
+            k = hex(address - 0x2)
             romData[k] = bytes(hex(int(romData[k], 16)+64)[2:].upper(), encoding='utf-8')
         j+=1
 
+
     random.shuffle(game.magi)
-    j=0
+    k=0
     for address in game.magiAddresses:
-        romData.update({ address : game.magi[j] })
+        romData.update({ hex(address) : game.magi[k] })
         #race MAGI duplication
-        if address == "0002AB55":
-            romData.update ({ "0002AAE0" : game.magi[j] })
-        elif address == "0002AB5B":
-            romData.update ({ "0002AB07"  : game.magi[j] })
-        elif address == "0002AB61":
-            romData.update ({"0002AB2F" : game.magi[j] })
-        j+=1
-    
-    for prices in game.newItemPrices:
-        romData.update(prices)
+        if address == 0x2ab55:
+            romData.update({ hex(174816) : game.magi[k] }) #0x2aae0
+        elif address == 0x2ab5b:
+            romData.update({ hex(174855) : game.magi[k] }) #0x2ab07
+        elif address == 0x2ab61:
+            romData.update({ hex(174895): game.magi[k] }) #0x2ab2f
+        k+=1
+        
+    for key, value in game.newItemPrices.items():
+        romData.update({hex(key) : value})
+
+    #shopshuffle
+    l=1
+    while l <= 10:
+        match l:
+            #final town
+            case 7:
+                a=0
+                t7 = list(game.shopTiers[7])
+                random.shuffle(t7)
+                while a < 8:
+                    romData.update({hex(260736+a) : t7[a]})
+                    a+=1
+            #recurring
+            case 8:
+                a=0
+                while a < 8:
+                    romData.update({hex(260744+a) : game.shopTiers[8][a]})
+                    a+=1
+            #echigoya
+            case 9:
+                echigoya = []
+                t0 = list(game.shopTiers[0])
+                t7 = list(game.shopTiers[7])
+                random.shuffle(t0)
+                random.shuffle(t7)
+                a = random.randint(0,5)
+                b = 6 - a
+                c = 0
+                while a >= 0:
+                    echigoya.append(t0[a])
+                    a-=1
+                while b >= 0:
+                    echigoya.append(t7[b])
+                    b-=1
+                while c < 8:
+                    romData.update({hex(260752+c) : echigoya[c]})
+                    c+=1
+            #giant town special
+            case 10:
+                gianttown = []
+                t0 = list(game.shopTiers[0])
+                t6 = list(game.shopTiers[6])
+                t7 = list(game.shopTiers[7])
+                random.shuffle(t0)
+                random.shuffle(t6)
+                random.shuffle(t7)
+                a = random.randint(3,6)
+                b = random.randint(0,2)
+                c = random.randint(0,2)
+                d = 0
+                while a > 0:
+                    gianttown.append(t0[a-1])
+                    a-=1
+                while b > 0 and len(gianttown) < 8:
+                    gianttown.append(t6[b-1])
+                    b-=1
+                while c > 0 and len(gianttown) < 8:
+                    gianttown.append(t7[c-1])
+                    c-=1
+                while d < 8:
+                    if d+1 <= len(gianttown):
+                        romData.update({hex(260760+d) : gianttown[d]})
+                    else:
+                        romData.update({hex(260760+d) : b"FF"})
+                    d+=1
+            case _:
+                addresses = []
+                for tierCheck in game.shopAddresses:
+                    if tierCheck[0] == l:
+                        addresses.append(tierCheck[1])
+                tier = list(game.shopTiers[l])
+                t0 = list(game.shopTiers[0])
+                nexttier = list(game.shopTiers[l+1])
+                random.shuffle(tier)
+                random.shuffle(t0)
+                t0 = t0[0:3]
+                random.shuffle(nexttier)
+                totalItems = random.randint((len(tier)+2), ((len(addresses)*8)))
+                currentShop = 0
+                positionTracker = 0
+                while totalItems > 0:
+                    if len(tier) > 0:
+                        romData.update({hex(addresses[currentShop]+positionTracker) : tier[0] })
+                        tier.pop(0)
+                    elif len(t0) > 0:
+                        romData.update({hex(addresses[currentShop]+positionTracker) : t0[0] })
+                        t0.pop(0)
+                    elif len(nexttier) > 0:
+                        romData.update({hex(addresses[currentShop]+positionTracker) : nexttier[0] })
+                        nexttier.pop(0)
+                    currentShop+=1
+                    if (currentShop > len(addresses)-1):
+                        currentShop = 0
+                        positionTracker+=1
+                    totalItems-=1
+                while positionTracker < 8:
+                    romData.update({hex(addresses[currentShop]+positionTracker) : b"FF" })
+                    currentShop+=1
+                    if (currentShop > len(addresses)-1):
+                        currentShop = 0
+                        positionTracker+=1
+        l+=1
 
     with open('Final Fantasy Legend 2 - ' + str(gameSeed) + '.gb', 'xb') as f:
         for key, value in romData.items():
--- FFL2R_data.py
+++ FFL2R_data.py
@@ -125,135 +125,135 @@ class GameData:
         b"61",
         b"13",
         b"6B"
-    ]
+        ]
 
     treasureAddresses = [
-        "0001C8F3",
-        "0001C8F9",
-        "0001C8FF",
-        "0001C905",
-        "0001CAB2",
-        "0001CAC5",
-        "0001CACB",
-        "0001CAD1",
-        "0001CAD7",
-        "0001CB0D",
-        "0001CB13",
-        "0001CB19",
-        "0001CB31",
-        "0001CBA5",
-        "0001CBD1",
-        "0001CBEA",
-        "0001CD8F",
-        "0001CDCA",
-        "0001CDE8",
-        "0001CE12",
-        "0001CE18",
-        "0001CE1E",
-        "0001CE24",
-        "0001CF2C",
-        "0001CF32",
-        "0001D10E",
-        "0001D114",
-        "0001D12E",
-        "0001D21E",
-        "0001D23C",
-        "0001D268",
-        "0001D29A",
-        "0001D2AD",
-        "0001D2B3",
-        "0001D2C6",
-        "0001D2D9",
-        "0001D4DC",
-        "0001D4E2",
-        "0001D4E8",
-        "0001D4EE",
-        "0001D4F4",
-        "0001D7C1",
-        "0001D7D8",
-        "0001D819",
-        "0001D81F",
-        "0001D8E5",
-        "0001D8EB",
-        "0001D907",
-        "0001D90D",
-        "0001D93A",
-        "0001DBDB",
-        "0001DBE1",
-        "0001DBE7",
-        "0001DBFA",
-        "0001DC0F",
-        "0001DC42",
-        "0001DD0A",
-        "0001DD35",
-        "0001DD3B",
-        "0001DF64",
-        "0001DF77",
-        "0001DF95",
-        "0001DFA8",
-        "0001DFBB",
-        "0001E0BC",
-        "0001E0CF",
-        "0001E0ED",
-        "0001E100",
-        "0001E113",
-        "0001D93A",
-        "0001DBDB",
-        "0001DBE1",
-        "0001DBE7",
-        "0001DBFA",
-        "0001DC0F",
-        "0001DC42",
-        "0001DD0A",
-        "0001DD35",
-        "0001DD3B",
-        "0001DF64",
-        "0001DF77",
-        "0001DF95",
-        "0001DFA8",
-        "0001DFBB",
-        "0001E0BC",
-        "0001E0CF",
-        "0001E0ED",
-        "0001E100",
-        "0001E113",
-        "0001E126",
-        "0001E144",
-        "0001E157",
-        "0001E3D6",
-        "0001E3DC",
-        "0001E3E2",
-        "0001E3E8",
-        "0001E3FB",
-        "0001E401",
-        "0001E407",
-        "0001E40D",
-        "0001E420",
-        "0001E426",
-        "0001E42C",
-        "0001E432",
-        "0001E445",
-        "0001E44B",
-        "0001E451",
-        "0001E457",
-        "0001E46A",
-        "0001E470",
-        "0001E476",
-        "0001E47C",
-        "0001E482",
-        "0001E495",
-        "0001E49B",
-        "0001E4A1",
-        "0001E4A7",
-        "0001E4BA",
-        "0001E4C0",
-        "0001E4C6",
-        "0001E4CC",
-        "0001E4E6",
-        "0001E4EC",
-        "0001E4F2",
-        "0001E4F8"
-    ]
+        0x1c8f3,
+        0x1c8f9,
+        0x1c8ff,
+        0x1c905,
+        0x1cab2,
+        0x1cac5,
+        0x1cacb,
+        0x1cad1,
+        0x1cad7,
+        0x1cb0d,
+        0x1cb13,
+        0x1cb19,
+        0x1cb31,
+        0x1cba5,
+        0x1cbd1,
+        0x1cbea,
+        0x1cd8f,
+        0x1cdca,
+        0x1cde8,
+        0x1ce12,
+        0x1ce18,
+        0x1ce1e,
+        0x1ce24,
+        0x1cf2c,
+        0x1cf32,
+        0x1d10e,
+        0x1d114,
+        0x1d12e,
+        0x1d21e,
+        0x1d23c,
+        0x1d268,
+        0x1d29a,
+        0x1d2ad,
+        0x1d2b3,
+        0x1d2c6,
+        0x1d2d9,
+        0x1d4dc,
+        0x1d4e2,
+        0x1d4e8,
+        0x1d4ee,
+        0x1d4f4,
+        0x1d7c1,
+        0x1d7d8,
+        0x1d819,
+        0x1d81f,
+        0x1d8e5,
+        0x1d8eb,
+        0x1d907,
+        0x1d90d,
+        0x1d93a,
+        0x1dbdb,
+        0x1dbe1,
+        0x1dbe7,
+        0x1dbfa,
+        0x1dc0f,
+        0x1dc42,
+        0x1dd0a,
+        0x1dd35,
+        0x1dd3b,
+        0x1df64,
+        0x1df77,
+        0x1df95,
+        0x1dfa8,
+        0x1dfbb,
+        0x1e0bc,
+        0x1e0cf,
+        0x1e0ed,
+        0x1e100,
+        0x1e113,
+        0x1d93a,
+        0x1dbdb,
+        0x1dbe1,
+        0x1dbe7,
+        0x1dbfa,
+        0x1dc0f,
+        0x1dc42,
+        0x1dd0a,
+        0x1dd35,
+        0x1dd3b,
+        0x1df64,
+        0x1df77,
+        0x1df95,
+        0x1dfa8,
+        0x1dfbb,
+        0x1e0bc,
+        0x1e0cf,
+        0x1e0ed,
+        0x1e100,
+        0x1e113,
+        0x1e126,
+        0x1e144,
+        0x1e157,
+        0x1e3d6,
+        0x1e3dc,
+        0x1e3e2,
+        0x1e3e8,
+        0x1e3fb,
+        0x1e401,
+        0x1e407,
+        0x1e40d,
+        0x1e420,
+        0x1e426,
+        0x1e42c,
+        0x1e432,
+        0x1e445,
+        0x1e44b,
+        0x1e451,
+        0x1e457,
+        0x1e46a,
+        0x1e470,
+        0x1e476,
+        0x1e47c,
+        0x1e482,
+        0x1e495,
+        0x1e49b,
+        0x1e4a1,
+        0x1e4a7,
+        0x1e4ba,
+        0x1e4c0,
+        0x1e4c6,
+        0x1e4cc,
+        0x1e4e6,
+        0x1e4ec,
+        0x1e4f2,
+        0x1e4f8
+        ]
     
     magi = [
         # 76 total. TrueEye (OA) is not a real Magi but instead a plot event that flips a script variable to make the Light Cave visible.
@@ -334,136 +334,163 @@ class GameData:
         b"0B", # Heart, 1
         b"0C", # Pegasus, 1
         b"0D" # Prism, 1 
-    ]
+        ]
 
     magiAddresses = [
-        "0001CB1F", #chests
-        "0001CB25",
-        "0001CB2B",
-        "0001CC08",
-        "0001CC0E",
-        "0001CC14",
-        "0001CF14",
-        "0001CF1A",
-        "0001D141",
-        "0001D147",
-        "0001D2EC",
-        "0001D2F2",
-        "0001D2F8",
-        "0001D840",
-        "0001D846",
-        "0001D84C",
-        "0001D858",
-        "0001D85E",
-        "0001D864",
-        "0001D928",
-        "0001D92E",
-        "0001E171",
-        "0002892A",	# Ki Assistant
-        "0002913A",	# Dad Giant World
-        "00029454",	# Apollo First
-        "00029E7E",	# Guardian World
-        "00029EA4",	 
-        "00029EAF",	 
-        "00029F39",	 
-        "0002993C",	# Ninja Cut Scenes
-        "0002A0A6",	 
-        "0002A275",	 
-        "0002A459",	# Nils's Return
-        "0002AB50",	# Race  
-        "0002AB55",	# NOTE: There are alternate drop methods dependent on dragon choice.
-        "0002AB5B",	# These three must be matched to their counterpart.
-        "0002AB61",	#
-        "0002AD20", # Magnate
-        "0002AD23",
-        "0002AD26",
-        "0002AD29",
-        "0002BA53",	# Dunatis
-        "0002BA56",	 
-        "0002BA59",	 
-        "0002C233", # Opening.      Anything from 2C000-2EB7F onward will need to have addresses offset by how many scripts are added *2.
-        "0002CAA6", # Ki's Body.    Current offset is 4.
-        "0002CAAF",
-        "0002CAB8",
-        "0002CAC1",
-        "0002CACA",
-        "0002CAD3",
-        "0002CADC",
-        "0002C874", # Odin
-        "0002C877",
-        "0002C87A",
-        "0002C87D",
-        "0002C880",
-        "0002C883",
-        "0002C886",
-        "0002DC58",
-        "0002D27B", #Nasty Dungeon
-        "0002E9E7", #Ashura
-        "0002E9EA",
-        "0002E9ED",
-        "0002E9F0",
-        "0002E9F3",
-        "0002E9F6",
-        "0002E9F9",
-        "0002E9FD", #Venus
-        "0002EA00", 
-        "0002EA03",
-        "0002EA06",
-        "0002EA09",
-        "0002EA0C",
-        "0002EA0F",
-        "0002CC74"
+        0x1cb1f, #chests
+        0x1cb25,
+        0x1cb2b,
+        0x1cc08,
+        0x1cc0e,
+        0x1cc14,
+        0x1cf14,
+        0x1cf1a,
+        0x1d141,
+        0x1d147,
+        0x1d2ec,
+        0x1d2f2,
+        0x1d2f8,
+        0x1d840,
+        0x1d846,
+        0x1d84c,
+        0x1d858,
+        0x1d85e,
+        0x1d864,
+        0x1d928,
+        0x1d92e,
+        0x1e171,
+        0x2892a, # Ki Assistant
+        0x2913a, # Dad Giant World
+        0x29454, # Apollo First
+        0x29e7e, # Guardian World
+        0x29ea4,	 
+        0x29eaf,	 
+        0x29f39,	 
+        0x2993c, # Ninja Cut Scenes
+        0x2a0a6,	 
+        0x2a275,	 
+        0x2a459, # Nils's Return
+        0x2ab50, # Race  
+        0x2ab55, # NOTE: There are alternate drop methods dependent on dragon choice.
+        0x2ab5b, # These three must be matched to their counterpart.
+        0x2ab61, #
+        0x2ad20, # Magnate
+        0x2ad23,
+        0x2ad26,
+        0x2ad29,
+        0x2ba53, # Dunatis
+        0x2ba56,	 
+        0x2ba59,	 
+        0x2c233, # Opening.      Anything from 2C000-2EB7F onward will need to have addresses offset by how many scripts are added *2.
+        0x2caa6, # Ki's Body.    Current offset is 4.
+        0x2caaf,
+        0x2cab8,
+        0x2cac1,
+        0x2caca,
+        0x2cad3,
+        0x2cadc,
+        0x2c874, # Odin
+        0x2c877,
+        0x2c87a,
+        0x2c87d,
+        0x2c880,
+        0x2c883,
+        0x2c886,
+        0x2dc58,
+        0x2d27b, #Nasty Dungeon
+        0x2e9e7, #Ashura
+        0x2e9ea,
+        0x2e9ed,
+        0x2e9f0,
+        0x2e9f3,
+        0x2e9f6,
+        0x2e9f9,
+        0x2e9fd, #Venus
+        0x2ea00, 
+        0x2ea03,
+        0x2ea06,
+        0x2ea09,
+        0x2ea0c,
+        0x2ea0f,
+        0x2cc74
         ]
 
-    magiAddressesAlternate = [
-        "0002AAE0", # 0002AB55
-        "0002AB07", # 0002AB5B
-        "0002AB2F"  # 0002AB61
-        ]
+    #magiAddressesAlternate = [
+    #    0x2aae0, # 2ab55
+    #    0x2ab07, # 2ab5b
+    #    0x2ab2f  # 2ab61
+    #    ]
 
-    newItemPrices = [
+    newItemPrices = {
         #prices are stored in three hex, addresses read right to left.
-        { "0003F929" : b"10" },  #stat boosting potions, changed to 10k each up from 1k
-        { "0003F92A" : b"27" },
-        { "0003F92B" : b"00" },
-        { "0003F92C" : b"10" },
-        { "0003F92D" : b"27" },
-        { "0003F92E" : b"00" },
-        { "0003F92F" : b"10" },
-        { "0003F930" : b"27" },
-        { "0003F931" : b"00" },
-        { "0003F932" : b"10" },
-        { "0003F933" : b"27" },
-        { "0003F934" : b"00" },
-        { "0003F9DA" : b"50" }, #selfix, changed to 50,000
-        { "0003F9DB" : b"C3" },
-        { "0003F9DC" : b"00" },
-        { "0003F9DD" : b"D1" }, #seven sword, changed to 77,777
-        { "0003F9DE" : b"2F" },
-        { "0003F9DF" : b"01" }
+        0x3f929 : b"88",  #stat boosting potions, changed to 5k each up from 1k
+        0x3f92a : b"13",
+        0x3f92b : b"00",
+        0x3f92c : b"88",
+        0x3f92d : b"13",
+        0x3f92e : b"00",
+        0x3f92f : b"88",
+        0x3f930 : b"13",
+        0x3f931 : b"00",
+        0x3f932 : b"88",
+        0x3f933 : b"13",
+        0x3f934 : b"00",
+        0x3f896 : b"50", #vampic, changed to 50,000
+        0x3f897 : b"C3",
+        0x3f898 : b"00",
+        0x3f9da : b"50", #selfix, changed to 50,000
+        0x3f9db : b"C3",
+        0x3f9dc : b"00",
+        0x3f9dd : b"D1", #seven sword, changed to 77,777
+        0x3f9de : b"2F",
+        0x3f9df : b"01"
+        }
+
+    shopAddresses = [
+        (1, 0x3f9e0), # 3f9e0-3f9e7 - First Town, Weapon Shop (tier 1)
+        (2, 0x3f9e8), # 3f9e8-3f9ef - Desert Town, Weapon Shop (tier 2)
+        (2, 0x3f9f0), # 3f9f0-3f9f7 - Ashura Town, Weapon Shop (tier 2)
+        (3, 0x3f9f8), # 3f9f8-3f9ff - Giant Town, Weapon Shop (tier 3)
+        (1, 0x3fa00), # 3fa00-3fa07 - Second Town, Weapon Shop (tier 1)
+        (3, 0x3fa08), # 3fa08-3fa0f - Apollo Town, Weapon Shop (tier 3)
+        (4, 0x3fa10), # 3fa10-3fa17 - Lynn Town, Weapon Shop (tier 4)
+        (4, 0x3fa18), # 3fa18-3fa1f - Guardian Town, Weapon Shop (tier 4)
+        (5, 0x3fa20), # 3fa20-3fa27 - Venus Town, Weapon Shop (tier 5)
+        (5, 0x3fa28), # 3fa28-3fa2f - Race Town, Weapon Shop (tier 5)
+        (6, 0x3fa30), # 3fa30-3fa37 - Edo Town, Weapon Shop (tier 6)
+        (6, 0x3fa38), # 3fa38-3fa3f - Final Town, Weapon Shop (tier 6)
+        (1, 0x3fa40), # 3fa40-3fa47 - First Town, Item Shop (tier 1)
+        (2, 0x3fa48), # 3fa48-3fa4f - Desert Town, Item Shop (tier 2)
+        (3, 0x3fa50), # 3fa50-3fa57 - Giant Town, Item Shop (tier 3)
+        (4, 0x3fa58), # 3fa58-3fa5f - Apollo Town, Item Shop (tier 4)
+        (4, 0x3fa60), # 3fa60-3fa67 - Guardian Town, Item Shop (tier 4)
+        (5, 0x3fa68), # 3fa68-3fa6f - Venus Town, Item Shop (tier 5)
+        (6, 0x3fa70), # 3fa70-3fa77 - Race Town, Item Shop (tier 6)
+        (6, 0x3fa78)  # 3fa78-3fa7f - Edo Town, Item Shop (tier 6)
+                      # 3fa80-3fa87 - Final Town, Item Shop (tier 7)
+                      # 3fa88-3fa8f - Recurring Item Shop (Second Town, Ashura Town, etc.) (tier 0)
+                      # 3fa90-3fa97 - Echigoya
+                      # 3fa98-3fa9f - Giant Town Giant Gear Seller
         ]
 
-    # 3F9E0-3F9E7 - First Town, Weapon Shop
-    # 3F9E8-3F9EF - Desert Town, Weapon Shop
-    # 3F9F0-3F9F7 - Ashura Town, Weapon Shop
-    # 3F9F8-3F9FF - Giant Town, Weapon Shop
-    # 3FA00-3FA07 - Second Town, Weapon Shop
-    # 3FA08-3FA0F - Apollo Town, Weapon Shop
-    # 3FA10-3FA17 - Lynn Town, Weapon Shop
-    # 3FA18-3FA1F - Guardian Town, Weapon Shop
-    # 3FA20-3FA27 - Venus Town, Weapon Shop
-    # 3FA28-3FA2F - Race Town, Weapon Shop 
-    # 3FA30-3FA37 - Edo Town, Weapon Shop
-    # 3FA38-3FA3F - Final Town, Weapon Shop
-    # 3FA40-3FA47 - First Town, Item Shop
-    # 3FA48-3FA4F - Desert Town, Item Shop
-    # 3FA50-3FA57 - Giant Town, Item Shop 
-    # 3FA58-3FA5F - Apollo Town, Item Shop
-    # 3FA60-3FA67 - Guardian Town, Item Shop
-    # 3FA68-3FA6F - Venus Town, Item Shop
-    # 3FA70-3FA77 - Race Town, Item Shop
-    # 3FA78-3FA7F - Edo Town, Item Shop
-    # 3FA80-3FA87 - Final Town, Item Shop
-    # 3FA88-3FA8F - Recurring Item Shop (Second Town, Ashura Town, etc.)
-    # 3FA90-3FA97 - Echigoya
-    # 3FA98-3FA9F - Giant Town Giant Gear Seller
+    #tier 1 x 3 = 24 slots, 14 items
+    #tier 2 x 3 = 24 slots, 16 items
+    #tier 3 x 3 = 24 slots, 16 items
+    #tier 4 x 4 = 32 slots, 15 items
+    #tier 5 x 3 = 24 slots, 15 items
+    #tier 6 x 4 = 32 slots, 15 items
+    #tier 7 x 1 = 8 slots, 13 items
+    #two specials
+    #w/six filler items
+
+    shopTiers = [
+        [b"47", b"24", b"2F", b"43", b"44", b"45", b"46"], #tent, cureb, healstaff, powerp, speedp, magicp, bodyp
+        [b"35", b"31", b"3A", b"17", b"00", b"01", b"15", b"48", b"4F", b"6D", b"2A", b"4C", b"56", b"67"], #bronze, hammer, longs, bow, whip, punch, psi knife, sleepb, colt, stung, coin
+        [b"18", b"32", b"36", b"3B", b"68", b"02", b"03", b"21", b"0F", b"50", b"26", b"27", b"28", b"29", b"4D", b"71"], #silver, kimono, axe, battles, abacus, rapier, kick, fireb, iceb, thunderb, fogb, musket, rocket]
+        [b"19", b"37", b"3C", b"64", b"33", b"04", b"05", b"55", b"49", b"16", b"51", b"25", b"2B", b"5A", b"5F", b"60"], #gold, armybody, katana, golds, temptat, blitzw, goldbow, headbutt, prayerb, stoneb, smg, fireg, missle]
+        [b"62", b"3D", b"63", b"65", b"0C", b"0A", b"0B", b"07", b"06", b"10", b"4B", b"6E", b"5B", b"5C", b"4A"], #giant, armyhelm, geta, thunderaxe, flames, ices, ogreaxe, corals, sabre, counter, psis, grenade, bazooka, chainsaw
+        [b"38", b"3F", b"73", b"1A", b"1B", b"0E", b"66", b"08", b"14", b"6C", b"52", b"2C", b"2D", b"4E", b"5D"], #dragonbody, hermes, gianthelm, flameshield, iceshield, runeaxe, sypha, dragons, revenge, lasers, x-kick, deathb, magestaff, magnum, vulcan
+        [b"75", b"40", b"3E", b"34", b"1C", b"69", b"0D", b"6A", b"09", b"11", b"53", b"2E", b"30", b"6F", b"5E"], #battlebody, hecate, ninjahand, dragonshield, samuraishield, defends, muramas, suns, catclaw, jyudo, wizardstaff, flareb, laserg, tankc
+        [b"39", b"76", b"7E", b"7F", b"6B", b"22", b"13", b"23", b"54", b"72", b"12", b"61", b"74"], #arthur, parasuit, selffix, sevens, gungnir, xcalibr, glasss, samuraibow, karate, psig, vampic, nukebomb, hyper
+        [b"1D", b"1E", b"1F", b"20", b"41", b"42", b"70", b"77"] #cure, xcure, curse, eyedrop, elixier, soft, speedup, door
+        ]
