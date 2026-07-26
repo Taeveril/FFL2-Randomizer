24d18caf3c2149e6e376921b9a667cb4e4ab0cd6
Harper S. <hsanford@renewfinancial.com>
2026-07-25 18:38:01 -0700

    Poison glitch actually fixed
    Points the poison death message call to one for when the attacker falls.

--- FFL2R_asm.py
+++ FFL2R_asm.py
@@ -8,10 +8,10 @@ class Fixes:
     def missingTrigger(rom:mmap):
         rom[0x1e292] = 0x09
 
-    #fixes the poison glitch by having _target_fell_message
-    #call cscript 0x07 instead of 0x24
+    # Fixes poison glitch by pointing the poison death message call to one specifically for when the attacker falls.
     def fixPoison(rom:mmap):
-        rom[0x31547] = 0x07
+        rom[0x30c19] = 0x7b
+        rom[0x30c1a] = 0x6c
 
     #elemental magi fix, mana magi affinity enable
     def magiFix(rom:mmap):
